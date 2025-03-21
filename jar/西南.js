681344fe253a41e5acbaabe4c5ca505fa7be3555
yuan301 <www.196301964@qq.com>
2025-03-21 15:47:26 +0000

    1
    Signed-off-by: yuan301 <www.196301964@qq.com>

---
+++ jar/西南.js
@@ -0,0 +1,11 @@
+{
+"分类url": "https://xn--44qz85a01qpc.com/pianku-{cateId}-{class}-{by}-{area}-{lang}----{catePg}---{year}.html",
+"分类": "电影$dianying#电视剧$dianshiju#综艺$zongyi#动漫$dongman",
+"影片年代": "<div class=\"module-info-tag\">&&</div>",
+"影片类型": "module-info-tag\">&&立即播放",
+"搜索url": "https://xn--44qz85a01qpc.com/search--------------.html?wd={wd}",
+"简介": "【刚哥YYDS】提醒您请勿相信影片中的广告，以免上当受骗！+style=\"line-height: 20px;\"&&</div>",
+"线路数组": "data-dropdown-value=&&</div>[不包含:QI节点#QT节点#KU节点]",
+"副标题": "💠刚哥带你看电视💠",
+"线路标题": "💠刚哥带你看电视💠+<span>&&</span>[排序:TK>HD>XS>YI>DI>]"
+}
---
+++ jar/酒酒.js
@@ -0,0 +1,12 @@
+{
+        "站名": "玖八",
+        "主页url": "http://www.98dyb.com/",
+        "简介": "【刚哥YYDS】提醒您请勿相信影片中的广告，以免上当受骗！+<span class=\"detail-content\" style=*>&&</span>",
+        "影片状态": "状态：&&</p>",
+        "副标题": "💠刚哥带你看电视💠",
+        "搜索url": "/search.php;post;searchword={wd}",
+        "线路数组": "<h3&&/h3>",
+        "线路标题": "刚哥带你看电视💠+src=*>&&<",
+        "分类url": "http://www.98dyb.com/{cateId}/index{catePg}.html[http://www.7xdy.com/{cateId}/index.html];;k",
+        "分类": "电影$dianyingpian#电视剧$dianshiju#综艺$zongyi#动漫$dongman"
+      }
