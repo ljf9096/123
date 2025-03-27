b32a3e4df9d3c47387e19b7bd154d754f2fe088f
yuan301 <www.196301964@qq.com>
2025-03-27 07:48:39 +0000

    删除文件 jar/爱影.js

--- jar/爱影.js
+++
@@ -1,5 +0,0 @@
-{
-        "副标题": "💠刚哥带你看电视💠",
-        "分类url": "https://www.iysdq.cc/vodshow/1-----------/area/{area}/by/{by}/class/{class}/id/{cateId}/page/{catePg}/year/{year}.html",
-        "分类": "电影$1#动漫$4#综艺$3"
-      }
